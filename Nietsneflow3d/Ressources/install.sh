47d677940474ff7a39f4b655b10d910b6dd7cb55
C <1203896@gmail.com>
2024-12-01 16:16:04 +0100

    Begin transition to run and gun.

--- Nietsneflow3d/Ressources/install.sh
+++
@@ -1,4 +0,0 @@
-cp Nietsneflow3d/Lib/lib* /usr/local/lib/
-cp bin/Arnihs /usr/local/bin/
-cp -r Nietsneflow3d/Ressources/ $HOME/.local/share/Arnihs
-cp Arnihs.desktop $HOME/.local/share/applications
--- Nietsneflow3d/launch.sh
+++
@@ -1,2 +0,0 @@
-clear
-LD_LIBRARY_PATH=./lib  ../build-Nietsneflow3d-Desktop-Release/Nietsneflow3d
