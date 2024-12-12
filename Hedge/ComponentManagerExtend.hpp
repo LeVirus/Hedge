51ca7e2a4c2df4159522fddc2723633d6322f25f
C <1203896@gmail.com>
2024-12-12 16:58:10 +0100

    Fix ECS container references issue.

--- Hedge/ComponentManagerExtend.hpp
+++ Hedge/ComponentManagerExtend.hpp
@@ -33,7 +33,10 @@ template <uint32_t N, Component_C... C>
 class ComponentManagerExtend : public ECS::ComponentsManager<N, C...>
 {
 public:
-    ComponentManagerExtend() = default;
+    ComponentManagerExtend()
+    {
+        reserveContainer(500);
+    }
     virtual ~ComponentManagerExtend() = default;
     virtual void instanciateComponentFromEntity(uint32_t numEntity, const std::array<uint32_t, N> &vect)override
     {
@@ -125,6 +128,34 @@ public:
         // std::vector<std::array<VectUI_t, N>> m_refComponents;
         // std::array<std::vector<uint32_t>, N> m_refDelComponents;
     }
+private:
+    void reserveContainer(uint32_t size)
+    {
+        //reserve all components container
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::POSITION_VERTEX_COMPONENT, PositionVertexComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::SPRITE_TEXTURE_COMPONENT, SpriteTextureComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::MEM_SPRITE_DATA_COMPONENT, MemSpriteDataComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::COLOR_VERTEX_COMPONENT, ColorVertexComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::MAP_COORD_COMPONENT, MapCoordComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::INPUT_COMPONENT, InputComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::CIRCLE_COLLISION_COMPONENT, CircleCollisionComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::SEGMENT_COLLISION_COMPONENT, SegmentCollisionComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::RECTANGLE_COLLISION_COMPONENT, RectangleCollisionComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::GENERAL_COLLISION_COMPONENT, GeneralCollisionComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::MOVEABLE_COMPONENT, MoveableComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::TIMER_COMPONENT, TimerComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::PLAYER_CONF_COMPONENT, PlayerConfComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::ENEMY_CONF_COMPONENT, EnemyConfComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::MEM_POSITIONS_VERTEX_COMPONENT, MemPositionsVertexComponents>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::WRITE_COMPONENT, WriteComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::SHOT_CONF_COMPONENT, ShotConfComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::OBJECT_CONF_COMPONENT, ObjectConfComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::WEAPON_COMPONENT, WeaponComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::AUDIO_COMPONENT, AudioComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::CHECKPOINT_COMPONENT, CheckpointComponent>().reserve(size);
+        ECS::ComponentsManager<N, C...>::template getVectTuple<Components_e::LOG_COMPONENT, LogComponent>().reserve(size);
+    }
+
     template <uint32_t numComponent, Component_C CC>
     void treatNewComponent(std::array<VectUI_t, N> &vectEntity, const std::array<uint32_t, N> &vect)
     {
