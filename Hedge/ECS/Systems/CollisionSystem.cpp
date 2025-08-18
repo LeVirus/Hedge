5d2f6f729b91ef1efdea186c325c4e6673b56c5c
C <1203896@gmail.com>
2025-08-18 14:39:16 +0200

    Fix stair bike animation.

--- Hedge/ECS/Systems/CollisionSystem.cpp
+++ Hedge/ECS/Systems/CollisionSystem.cpp
@@ -1611,7 +1611,7 @@ void CollisionSystem::collisionRectTriangleEject(CollisionArgs &args, bool down)
                 VehicleComponent *vehicleComp = Ecsm_t::instance().getComponent<VehicleComponent, Components_e::VEHICLE_COMPONENT>(args.entityNumA);
                 assert(vehicleComp);
                 //if onstair ==> sprite already treated for this frame
-                if((!vehicleComp->m_onStair && std::abs(diffY) > EPSILON_FLOAT) || std::abs(diffX) == 30)
+                if(std::abs(diffY) > EPSILON_FLOAT && (!vehicleComp->m_onStair || std::abs(diffX) == 30))
                 {
                     updateVehicleSpriteType(down);
                 }
