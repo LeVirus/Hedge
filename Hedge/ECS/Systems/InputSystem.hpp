58760338130832401cbd57a4a0297c6e87db0600
LeVirus <1203896@gmail.com>
2026-03-16 18:48:40 +0100

    Fix default gamepad inputs.

--- Hedge/ECS/Systems/InputSystem.hpp
+++ Hedge/ECS/Systems/InputSystem.hpp
@@ -106,16 +106,16 @@ inline const std::map<ControlKey_e, MouseKeyboardInputState> MAP_KEYBOARD_DEFAUL
 
 inline const std::map<ControlKey_e, GamepadInputState> MAP_GAMEPAD_DEFAULT_KEY =
     {
-    {ControlKey_e::LOOK_UP, GamepadInputState{false, GLFW_GAMEPAD_AXIS_LEFT_Y, true}},
-    {ControlKey_e::LOOK_DOWN, GamepadInputState{false, GLFW_GAMEPAD_AXIS_LEFT_Y, false}},
-    {ControlKey_e::AIM_DIAG_UP, GamepadInputState{false, GLFW_GAMEPAD_AXIS_LEFT_X, false}},
-    {ControlKey_e::AIM_DIAG_DOWN, GamepadInputState{false, GLFW_GAMEPAD_AXIS_LEFT_X, true}},
-    {ControlKey_e::MOVE_LEFT, GamepadInputState{false, GLFW_GAMEPAD_AXIS_RIGHT_X, false}},
-    {ControlKey_e::MOVE_RIGHT, GamepadInputState{false, GLFW_GAMEPAD_AXIS_RIGHT_X, true}},
-    {ControlKey_e::JUMP, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_X, {}}},
-    {ControlKey_e::ACTION, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_A, {}}},
-    {ControlKey_e::SHOOT, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, {}}},
-    {ControlKey_e::GRENADE, {true, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER}},
+    {ControlKey_e::LOOK_UP, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_DPAD_UP, {}}},
+    {ControlKey_e::LOOK_DOWN, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_DPAD_DOWN, {}}},
+    {ControlKey_e::AIM_DIAG_UP, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_LEFT_BUMPER, {}}},
+    {ControlKey_e::AIM_DIAG_DOWN, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_RIGHT_BUMPER, {}}},
+    {ControlKey_e::MOVE_LEFT, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_DPAD_LEFT, {}}},
+    {ControlKey_e::MOVE_RIGHT, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_DPAD_RIGHT, {}}},
+    {ControlKey_e::JUMP, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_A, {}}},
+    {ControlKey_e::ACTION, GamepadInputState{false, GLFW_GAMEPAD_AXIS_RIGHT_TRIGGER, true}},
+    {ControlKey_e::SHOOT, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_X, {}}},
+    {ControlKey_e::GRENADE, GamepadInputState{false, GLFW_GAMEPAD_AXIS_LEFT_TRIGGER, true}},
     {ControlKey_e::PREVIOUS_WEAPON, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_Y, {}}},
     {ControlKey_e::NEXT_WEAPON, GamepadInputState{true, GLFW_GAMEPAD_BUTTON_B, {}}}
 };
